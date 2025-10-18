1f5294f3c77210b6842df8df4a576791d6269122
Dedar <thededar@gmail.com>
2025-10-18 03:45:56 +0300

    fix(footer): remove unnecessary whitespace in Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center justify-center text-center">
           {/* <div className="text-5xl font-bold mb-4">TechFolk</div> */}
-          
+
           <div className="flex space-x-6 mb-6">
             <a href="#" className="hover:text-background/80 transition-colors">
               Twitter
@@ -16,12 +16,12 @@ export function Footer() {
               GitHub
             </a>
           </div>
-          
+
           <div className="text-background/60 text-sm">
             © 2025 TechFolk. All rights reserved.
           </div>
         </div>
       </div>
     </footer>
-  )
+  );
 }
